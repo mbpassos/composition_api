d580cd9bf9b09be79cb2754d5c087ba3e7a28d84
mbpassos <marilia.bpassos@gmail.com>
2021-05-20 12:03:11 +0100

    basic setup example

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,24 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    home
   </div>
+  <p>My name is {{ name }} and my age is {{ age }}</p>
+  <button @click="handleClick">click me</button>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
+  setup() {
+    let name = 'mario'
+    let age = 30
+
+    const handleClick = () => {
+      console.log('you clicked me')
+    }
+
+    return { name, age, handleClick }
+    
   }
 }
 </script>
