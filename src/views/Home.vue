93d57e754979fa4c05d2b5a567d38f9b44d12d53
mbpassos <marilia.bpassos@gmail.com>
2021-05-20 14:27:47 +0100

    watch $ watch effect

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,22 +6,36 @@
     <div v-for="name in matchingNames" :key="name">
       <p>{{ name }}</p>
     </div>
+    <button @click="handleClick">Stop Watching</button>
   </div>  
 </template>
 
 <script>
-import { computed, ref } from 'vue'
+import { computed, ref, watch, watchEffect } from 'vue'
 export default {
   name: 'Home',
   setup() {
     const search = ref('')
     const names = ref(['mario', 'antonio', 'nuno', 'gonçalo', 'joão', 'miguel'])
 
+    const stopWatch = watch(search, () => {
+      console.log('watch function run')
+    })
+
+    const stopEffect = watchEffect(() => {
+      console.log('watch effect funcion run', search.value)
+    })
+
+    const handleClick = () => {
+      stopWatch()
+      stopEffect()
+    }
+
     const matchingNames = computed(()=>{
       return names.value.filter((name) => name.includes(search.value))
     })
 
-    return { names, search, matchingNames }  
+    return { names, search, matchingNames, stopWatch, stopEffect, handleClick }  
     
   }
 }
