fc055ffd14b00f6feccb0d07ce88019ac726972a
mbpassos <marilia.bpassos@gmail.com>
2021-05-20 12:23:39 +0100

    refs for reactive values

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,8 +2,10 @@
   <div class="home">
     home
   </div>
-  <p ref="p">My name is {{ name }} and my age is {{ age }}</p>
+  <p>My name is {{ name }} and my age is {{ age }}</p>
   <button @click="handleClick">click me</button>
+  <input type="text" v-model="name">
+  <button @click="age++">add 1 to age</button>
 </template>
 
 <script>
@@ -11,18 +13,16 @@ import { ref } from 'vue'
 export default {
   name: 'Home',
   setup() {
-    let name = 'mario'
-    let age = 30
+    const name = ref('mario')
+    const age = ref(30)    
 
-    const p = ref(null)
-
+    
     const handleClick = () => {
-      console.log(p.value)
-      p.value.classList.add('test')
-      p.value.textContent = 'hello, ninjas'
+      name.value = 'luigi'
+      age.value = 35
     }
 
-    return { name, age, handleClick, p }
+    return { name, age, handleClick }
     
   }
 }
